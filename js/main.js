c17c895b2a84975dcd5d7b3c1a97572698cc92b2
OlegMorshel <GELOROM@bk.ru>
2022-01-30 13:58:04 +0300

    loading a project

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(function () {
+	$('.slider__box').slick({
+		prevArrow: '<img class="slider__arrow slider__arrow-left" src="/images/main/info/arrow-left.svg" alt="arrow-left">',
+		nextArrow: '<img class="slider__arrow slider__arrow-right"  src="/images/main/info/arrow-right.svg" alt="arrow-right">',
+		
+		responsive: [
+			{
+				breakpoint: 541,
+				settings: {
+					arrows: false
+				}
+			}
+		]
+	});	
+
+	$('.menu__list-btn').on('click', function () {
+		$('.menu__list').toggleClass('active');
+	});
+
+});
