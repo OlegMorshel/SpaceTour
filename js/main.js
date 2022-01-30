2e2f21d04337c02136013b6f906093dd552cec6a
OlegMorshel <GELOROM@bk.ru>
2022-01-30 16:40:15 +0300

    change arrows in slider

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(function () {
 	$('.slider__box').slick({
-		prevArrow: '<img class="slider__arrow slider__arrow-left" src="/images/main/info/arrow-left.svg" alt="arrow-left">',
-		nextArrow: '<img class="slider__arrow slider__arrow-right"  src="/images/main/info/arrow-right.svg" alt="arrow-right">',
+		prevArrow: '<img class="slider__arrow slider__arrow-left" src="./images/main/info/arrow-left.svg" alt="arrow-left">',
+		nextArrow: '<img class="slider__arrow slider__arrow-right"  src="./images/main/info/arrow-right.svg" alt="arrow-right">',
 		
 		responsive: [
 			{
